c22df68ace9f8b18f09ef15bd869f9dae0ce8ee3
Thomas Levine <occurrence@thomaslevine.com>
2012-11-08 11:02:35 -0500

    data summaries

--- eda.r
+++ eda.r
@@ -1,21 +1,47 @@
 #!/usr/bin/env Rscript
 
+# Plot a factor all pretty-like
+p <- function(x, filename, xlab = '', ...) {
+  png(filename, width=1600, height=900)
+  
+  # Margin
+  par(oma = c(10, 0, 0, 0))
+  
+  plot(
+    x,
+
+    # xlab goes below
+    xlab = '',
+  
+    # Perpendicular labels
+    las = 2,
+
+    ...
+  )
+  mtext(xlab, 1, 10)
+  
+  dev.off()
+}
+
+# Load the data.
 w <- read.csv('wetlands.csv')
 
-png('plots/parish.png', width=1600, height=900)
-
-# Margin
-par(oma = c(7, 0, 0, 0))
-
-plot(
+# Parishes
+p(
   w$Parish,
+  'plots/parish.png',
   main = 'Permit applications by parish',
-  xlab = '',
   ylab = 'Number of permit applications',
-
-  # Perpendicular labels
-  las = 2
+  xlab = "Parish"
 )
-mtext("Parish", 1, 7)
 
-dev.off()
+# Project managers
+p(
+  w$Project.Manager.Name,
+  'plots/project_manager.png',
+  main = 'Permit applications by project manager',
+  ylab = 'Number of permit applications',
+  xlab = "Project manager"
+)
+
+print(summary(w[14:18]))

60eb01a716ad4517fa095722560a10b8e4115104
Thomas Levine <occurrence@thomaslevine.com>
2012-11-08 11:30:48 -0500

    another plot

--- eda.r
+++ eda.r
@@ -2,7 +2,7 @@
 
 # Plot a factor all pretty-like
 p <- function(x, filename, xlab = '', ...) {
-  png(filename, width=1600, height=900)
+  png(filename, width=1600, height=900, font = 'Helvetica')
   
   # Margin
   par(oma = c(10, 0, 0, 0))
@@ -44,4 +44,28 @@ p(
   xlab = "Project manager"
 )
 
-print(summary(w[14:18]))
+# Public notice dates
+# p(w$Public.Notice.Date, 'plots/public_notice_date.png')
+.d <- as.data.frame(table(w$Public.Notice.Date))
+colnames(.d) <- c('Date', 'Number.of.notices')
+.d$Date <- as.Date(.d$Date)
+.d$Cumulative.notices <- cumsum(.d$Number.of.notices)
+
+png('plots/public_notice_date.png', width=1600, height=900, pointsize = 18, font = 'Helvetica')
+plot(
+  Cumulative.notices ~ Date, data = .d,
+  xlim = as.Date(c('2012-07-01', '2012-10-01')),
+  main = 'When were these notices posted?',
+  axes = F,
+  bg = 1,
+  col = 0,
+  pch = 21,
+  cex = Number.of.notices * 4/5
+)
+axis(2)
+.dates <- as.Date(c('2012-07-01', '2012-07-15', '2012-08-01', '2012-08-15', '2012-09-01', '2012-09-15', '2012-10-01'))
+.names <- strftime(.dates, '%B %d')
+axis(1, at = .dates, labels = .names)
+dev.off()
+
+# print(summary(w[14:18]))
